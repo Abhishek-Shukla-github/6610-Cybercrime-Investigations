ab26a9c738641a3d54639fceffadc4ad9ce00f57
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-06-13 00:44:44 -0400

    Add Firearm protection law

---
+++ firearm_protection.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+# from explosive import log
+
+# Step 1: Send a request to the web page
+url = 'https://www.legisquebec.gouv.qc.ca/en/document/cs/P-38.0001'  # Use your actual URL
+# url = 'https://www.legisquebec.gouv.qc.ca/en/document/cs/E-22'  # Use your actual URL
+headers = {'User-Agent': 'Mozilla/5.0'}
+response = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+finalObj = {}
+label_groups = soup.select(".Heading.Heading .Label-group4")
+header_divs = soup.select(".Heading.Heading")
+obj = {}
+
+def fetch_firearm_protection_laws():
+    if response.status_code == 200:
+        return fetchChapters()
+    else:
+        print(f"Failed to retrieve the web page. Status code: {response.status_code}")
+    
+
+def fetchChapters():
+    chapters = soup.select(".LegislativeDocument.Global.Style_Extra_Class")
+    for i,chapter in enumerate(chapters):
+        descriptions = chapter.select(".section")
+        title_num = "chapter R-14"
+        title_name = "ACT TO PROTECT PERSONS WITH REGARD TO ACTIVITIES INVOLVING FIREARMS"
+        section_array = []
+        for section in descriptions:
+            single_section = section.select_one(".Subsection").get_text() 
+            section_array.append(single_section)
+            
+        obj[title_num] ={
+            "title": title_name,
+        }
+        obj[title_num]["sections"] = section_array
+        # print(obj)
+    return {"ACT TO PROTECT PERSONS WITH REGARD TO ACTIVITIES INVOLVING FIREARMS": obj}
+
+# fetch_firearm_protection_laws()
--- script.py
+++ script.py
@@ -5,12 +5,13 @@ from anticorruption import fetch_anticorruption_laws
 from explosive import fetch_explosive_laws
 from tear_bombs import fetch_tear_bomb_laws
 from syndical import fetch_syndical_laws
+from firearm_protection import fetch_firearm_protection_laws
 
 # Step 2: Check if the request was successful
     # Step 3: Parse the HTML content
 # anticorruption_result = fetch_anticorruption_laws()
 # explosive_laws_result = fetch_explosive_laws()
-finalObj = [fetch_anticorruption_laws(), fetch_explosive_laws(), fetch_tear_bomb_laws(), fetch_syndical_laws()]
+finalObj = [fetch_anticorruption_laws(), fetch_explosive_laws(), fetch_tear_bomb_laws(), fetch_syndical_laws(),fetch_firearm_protection_laws()]
 
 
 print(json.dumps(finalObj))
