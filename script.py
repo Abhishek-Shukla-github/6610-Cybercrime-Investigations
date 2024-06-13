bbd77ff4bc6eff94eac2b5581b125b4b98c441a4
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-06-13 00:11:09 -0400

    Add Tear Bombs

--- script.py
+++ script.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 import json
 from anticorruption import fetch_anticorruption_laws
 from explosive import fetch_explosive_laws
+from tear_bombs import fetch_tear_bomb_laws
 
 # Step 2: Check if the request was successful
     # Step 3: Parse the HTML content
 # anticorruption_result = fetch_anticorruption_laws()
 # explosive_laws_result = fetch_explosive_laws()
-finalObj = [fetch_anticorruption_laws(), fetch_explosive_laws()]
+finalObj = [fetch_anticorruption_laws(), fetch_explosive_laws(), fetch_tear_bomb_laws()]
 
 
 print(json.dumps(finalObj))
